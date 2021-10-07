193ff14cc6cf948420f6d66ccace1654149aa14d
Lucas Trindade <nh.lucass@gmail.com>
2021-10-07 19:01:51 -0300

    ms

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
-import Login from './pages/Login';
+import history from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    // Envolve as rotas dentro de BrowserRoutes e controla os links
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyle />
-    </>
+    </Router>
   );
 }
 
